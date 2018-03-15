940b70f1a27159bbe2c9114068dabd0bf6b37e93
wq521193913 <521193913@qq.com>
2018-03-15 22:10:06 +0800

    1

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ App({
     // 登录
     wx.login({
       success: res => {
-        
-        this.globalData.code=res.code;
-        console.log(res);
-        console.log(this.globalData.userInfo);
         wx.request({
           url: this.serverUrl + "/static/wxLogin",
           method:'POST',
@@ -38,8 +34,6 @@ App({
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
-              console.log(222);
-              console.log(this.globalData.code);
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
 
