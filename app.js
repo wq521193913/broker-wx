677eac2156ca6ecddb0723d72a3f86cdf8dec8c2
wq521193913 <521193913@qq.com>
2018-03-16 18:01:25 +0800

    1

--- app.js
+++ app.js
@@ -9,45 +9,65 @@ App({
       }
     });
   },
+  wxUserInfo: function(cb){
+    wx.getUserInfo({
+      success: res => {
+        this.globalData.userInfo = res.userInfo;
+        this.globalData.encryptedData = res.encryptedData;
+      }
+    })
+  },
+  userLogin: function(cb){
+    
+    if (!this.globalData.code) {
+      this.wxLogin();
+    }
+    if (!this.globalData.userInfo){
+      this.wxUserInfo();
+    }
+
+    if (!this.globalData.code || !this.globalData.userInfo){
+      setInterval("wxGetUserInfo()", 50);
+      return;
+    }
+    wx.request({
+      url: this.serverUrl + "/static/wxLogin",
+      method: 'POST',
+      data: {
+        code: this.globalData.code,
+        wxNickName: this.globalData.userInfo.nickName,
+        wxCity: this.globalData.userInfo.city,
+        wxProvince: this.globalData.userInfo.province,
+        wxCountry: this.globalData.userInfo.country,
+        wxAvatarUrl: this.globalData.userInfo.nickName,
+        encryptedData: encryptedData
+      },
+      dataType: 'json',
+      success: function (res) {
+        app.globalData.session_3rd = res.data;
+      },
+      fail: function (res) {
+        console.log(res);
+      }
+    })
+  },
+  
   onLaunch: function () {
     // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
 
     // 登录
     this.wxLogin();
     // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userLocation']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-              
-            }
-          });
-
-          
-        }
-      }
-    })
+    
   },
+
   
 
   globalData: {
     userInfo: null,
     code: null,
-    session_3rd:null
+    session_3rd:null,
+    encryptedData:null
   },
   
 })
